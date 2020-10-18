279fb75dee447f5ac131c0a9902e6c32e4618a8c
ketanpatil2211 <48654668+ketanpatil2211@users.noreply.github.com>
2020-10-18 11:00:15 +0530

    identity

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -7,10 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
 
-
-    [AllowAnonymous] //overwrites the authorization in policy bcz of the lower level
     public class UserController : BaseController
     {
+        [AllowAnonymous] //overwrites the authorization in policy bcz of the lower level
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login([FromBody] Login.Query query)
         {
@@ -18,5 +17,17 @@ namespace API.Controllers
             return await Mediator.Send(query);
         }
 
+        [AllowAnonymous] //overwrites the authorization in policy bcz of the lower level
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register([FromBody] Register.Command command)
+        {
+            return await Mediator.Send(command);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> CurrentUser()
+        {
+            return await Mediator.Send(new CurrentUser.Query());
+        }
     }
 }
